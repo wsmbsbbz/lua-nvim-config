09a7a48cb6581499c81d214c3dc423ee7164a486
wsmbsbbz <wsmbsbbz@gmail.com>
2022-06-03 11:53:48 +0800

    Update nvim-tree.lua: migrate nvim-tree options
    see https://github.com/kyazdani42/nvim-tree.lua/issues/674

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,37 +1,5 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
-
-vim.g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1,
-  folder_arrows = 1,
-  tree_width = 30,
-}
-
-vim.g.nvim_tree_root_folder_modifier = ":t";
-vim.g.nvim_tree_git_hl = true;
-
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
@@ -45,6 +13,46 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
+  renderer = {
+    icons = {
+      webdev_colors = true,
+      git_placement = "before",
+      padding = " ",
+      symlink_arrow = " ➛ ",
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        },
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+        },
+      },
+    },
+    highlight_git = true,
+    root_folder_modifier = ":t",
+  },
+  hijack_directories = {
+    enable = true,
+    auto_open = true,
+  },
   disable_netrw = true,
   hijack_netrw = true,
   open_on_setup = false,
