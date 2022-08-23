c9d329f7e76e1670dc6e4fb8f7d9aa95b940eff6
wsmbsbbz <wsmbsbbz@gmail.com>
2022-08-23 14:38:59 +0800

    Update config.lua: add symbols-outline.nvim plugin

--- config.lua
+++ config.lua
@@ -69,6 +69,9 @@ lvim.builtin.which_key.mappings["de"] = {
 lvim.builtin.which_key.mappings["dE"] = {
   "<cmd>lua require'dapui'.eval(vim.fn.input '[Expression] > ')<cr>", "Evaluate Input"
 }
+lvim.builtin.which_key.mappings["o"] = {
+  "<cmd>SymbolsOutline<cr>", "SymbolsOutline"
+}
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -194,6 +197,71 @@ lvim.plugins = {
     config = function()
       require("dapui").setup {}
     end
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    config = function()
+      require("symbols-outline").setup({
+        highlight_hovered_item = true,
+        show_guides = true,
+        auto_preview = false,
+        position = 'right',
+        relative_width = true,
+        width = 25,
+        auto_close = false,
+        show_numbers = false,
+        show_relative_numbers = false,
+        show_symbol_details = true,
+        preview_bg_highlight = 'Pmenu',
+        autofold_depth = nil,
+        auto_unfold_hover = true,
+        fold_markers = { '', '' },
+        keymaps = { -- These keymaps can be a string or a table for multiple keys
+          close = { "<Esc>", "q" },
+          goto_location = "<Cr>",
+          focus_location = "o",
+          hover_symbol = "<C-space>",
+          toggle_preview = "K",
+          rename_symbol = "r",
+          code_actions = "a",
+          fold = "h",
+          unfold = "l",
+          fold_all = "W",
+          unfold_all = "E",
+          fold_reset = "R",
+        },
+        lsp_blacklist = {},
+        symbol_blacklist = {},
+        symbols = {
+          File = { icon = "", hl = "TSURI" },
+          Module = { icon = "", hl = "TSNamespace" },
+          Namespace = { icon = "", hl = "TSNamespace" },
+          Package = { icon = "", hl = "TSNamespace" },
+          Class = { icon = "𝓒", hl = "TSType" },
+          Method = { icon = "ƒ", hl = "TSMethod" },
+          Property = { icon = "", hl = "TSMethod" },
+          Field = { icon = "", hl = "TSField" },
+          Constructor = { icon = "", hl = "TSConstructor" },
+          Enum = { icon = "ℰ", hl = "TSType" },
+          Interface = { icon = "ﰮ", hl = "TSType" },
+          Function = { icon = "", hl = "TSFunction" },
+          Variable = { icon = "", hl = "TSConstant" },
+          Constant = { icon = "", hl = "TSConstant" },
+          String = { icon = "𝓐", hl = "TSString" },
+          Number = { icon = "#", hl = "TSNumber" },
+          Boolean = { icon = "⊨", hl = "TSBoolean" },
+          Array = { icon = "", hl = "TSConstant" },
+          Object = { icon = "⦿", hl = "TSType" },
+          Key = { icon = "🔐", hl = "TSType" },
+          Null = { icon = "NULL", hl = "TSType" },
+          EnumMember = { icon = "", hl = "TSField" },
+          Struct = { icon = "𝓢", hl = "TSType" },
+          Event = { icon = "🗲", hl = "TSType" },
+          Operator = { icon = "+", hl = "TSOperator" },
+          TypeParameter = { icon = "𝙏", hl = "TSParameter" }
+        }
+      })
+    end,
   },
 }
 
