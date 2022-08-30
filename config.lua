31f4d9b062209b15969a6c4dcc40a6e00c283275
wsmbsbbz <wsmbsbbz@gmail.com>
2022-08-30 17:30:11 +0800

    Update config.lua: disable direct interaction with the system clipboard

--- config.lua
+++ config.lua
@@ -12,6 +12,7 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save = false
 lvim.colorscheme = "OceanicNext"
+vim.opt.clipboard = ""
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
