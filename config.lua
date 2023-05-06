1e0946588fac7fbd58281c1e529994cd9298bd7d
wsmbsbbz <wsmbsbbz@gmail.com>
2023-05-06 21:13:00 +0800

    Update config.lua: add which-key shortcut for yank buffer to sys cilpborad

--- config.lua
+++ config.lua
@@ -64,6 +64,7 @@ lvim.builtin.cmp.completion.keyword_length = 0
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["<space>"] = { "", "NULL" }
+lvim.builtin.which_key.mappings["y"] = { "<cmd>%yank +<CR>", "Yank buffer" }
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 -- lvim.builtin.which_key.mappings["t"] = {
 --   name = "+Trouble",
