780030e519c5d088a717fc7bbcf253965e72706c
wsmbsbbz <wsmbsbbz@gmail.com>
2023-06-20 21:25:30 +0800

    Update config.lua: set relativenumber on

--- config.lua
+++ config.lua
@@ -12,6 +12,7 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save = false
 lvim.colorscheme = "tokyonight"
+vim.opt.relativenumber = true
 vim.opt.clipboard = ""
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
